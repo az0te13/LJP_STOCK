1cc45af16f8206e6e723da0f1a03199cbf770927
az0te13 <gabriel.ciullo@rpn.ch>
2016-12-08 21:52:03 +0100

    m

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name stockljpApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the stockljpApp
+ */
+angular.module('stockljpApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
